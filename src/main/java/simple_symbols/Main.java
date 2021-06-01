9c91464cdcab61801670ec5d7374e41e41fdbef2
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-06-01 15:21:00 +0200

    more tasks

---
+++ src/main/java/simple_symbols/Main.java
@@ -0,0 +1,24 @@
+package simple_symbols;
+
+public class Main {
+
+    public static String SimpleSymbols(String str) {
+        // code goes here
+        if(Character.isLetter(str.charAt(0)) || Character.isLetter(str.charAt(str.length()-1))){
+            return "false";
+        }
+        for (int i = 1; i < str.length()-1; i++) {
+            if(Character.isLetter(str.charAt(i))){
+                if(!(str.charAt(i-1) == '+' && str.charAt(i+1) == '+')){
+                    return "false";
+                }
+            }
+        }
+        return "true";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SimpleSymbols("+d===+a+"));
+    }
+
+}
