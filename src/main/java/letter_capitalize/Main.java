7d5f72aba66c20e05452ddf13242e372449180ba
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-06-01 14:57:06 +0200

    more tasks

---
+++ src/main/java/letter_capitalize/Main.java
@@ -0,0 +1,17 @@
+package letter_capitalize;
+
+public class Main {
+
+    public static String LetterCapitalize(String str) {
+        // code goes here
+        String[] strSplit = str.split("\\s");
+        StringBuilder solution = new StringBuilder();
+        for(String elem : strSplit){
+            solution.append(Character.toUpperCase(elem.charAt(0))).append(elem.substring(1)).append(" ");
+        }
+
+        return solution.deleteCharAt(solution.length()-1).toString();
+    }
+
+
+}
