4e423ff5c707f467a6e5e503a93654ae475ea578
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-06-05 19:22:44 +0200

    more tasks

---
+++ src/main/java/run_length/Main.java
@@ -0,0 +1,24 @@
+package run_length;
+
+public class Main {
+
+
+    public static String RunLength(String str) {
+        // code goes here
+        StringBuilder solution = new StringBuilder();
+        int i = 0;
+        while (i < str.length()) {
+            int repeated = 1;
+            while(i + repeated < str.length() && str.charAt(i + repeated) == str.charAt(i)) {
+                repeated++;
+            }
+            solution.append(repeated).append(str.charAt(i));
+            i += repeated;
+        }
+        return solution.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RunLength("aabbccdd"));
+    }
+}
