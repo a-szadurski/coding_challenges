f485bff5b44da29c2aaeef84f530b9fc8a8c1718
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-06-15 15:30:29 +0200

    tasks

---
+++ src/main/java/binary/BinaryConverterSimple.java
@@ -0,0 +1,10 @@
+package binary;
+
+public class BinaryConverterSimple {
+
+    public static int BinaryConverter(String str) {
+        // code goes here
+        return Integer.parseInt(str,2);
+    }
+
+}
